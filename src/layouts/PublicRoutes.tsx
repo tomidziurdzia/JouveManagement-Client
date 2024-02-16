9fd7ea9460da42527f952d92e43b7029368a79d4
Tomas Dziurdzia <tomidziurdzia@gmail.com>
2024-02-16 13:06:48 -0300

    saque el logo

--- src/layouts/PublicRoutes.tsx
+++ src/layouts/PublicRoutes.tsx
@@ -12,7 +12,7 @@ const PublicRoutes = () => {
     <div className="flex w-full justify-center mt-5 md:mt-10">
       <div className="flex md:w-2/3 mx-4 md:mx-0 lg:w-1/3 flex-col justify-center p-5 md:p-10 shadow rounded-md bg-white">
         <div className="flex justify-center items-center mb-5">
-          <img src="/public/logoCompleto.png" className="w-3/4" />
+          {/* <img src="/public/logoCompleto.png" className="w-3/4" /> */}
         </div>
         <div className="w-full">
           <div className="flex justify-center">
